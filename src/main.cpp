67fd5370225d5403f235d07c0acd2113b0240221
samansepehri <samansepehri@gmail.com>
2019-09-30 22:48:01 -0600

    default scene and objects.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <GLUT/GLUT.h>
 #include <OpenGL/OpenGL.h>
-#include "tinyxml/tinyxml.h"
+
 #include "scene.h"
+#include "objects.h"
+#include "viewport.cpp"
+#include "xmlload.cpp"
 
 using namespace std;
 
-int main()
+Node rootNode;
+Camera camera;
+RenderImage renderImage;
+Sphere theSphere;
+
+void BeginRender()
 {
-    
+
+}
+
+void StopRender()
+{
+
+}
+
+int main()
+{   
+    ShowViewport();
     cout << "Hello World! \n";
     return 0;
 }
